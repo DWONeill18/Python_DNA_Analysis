0ec9d10678a01bc1ebe62d9b531dfbafc7baf6fe
DWONeill18 <d.w.oneill.18@gmail.com>
2020-06-01 20:10:49 +0100

    Added reverse_transcription method

--- dna_analysis.py
+++ dna_analysis.py
@@ -247,6 +247,9 @@ def mutation(dna_strand):
     else:
         print("File is invalid.")
 
+# Method for reverse transcription
+def reverse_transcription(rna_strand):
+    pass
 
 def welcome():
     print("############################################")
